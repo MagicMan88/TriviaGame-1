25bb045e2ef0f36f3684828cee3cf6f1dfbafdc1
Stephen Green <stephengreen1028@gmail.com>
2019-11-13 21:31:52 -0700

    rewriting form on index.html, restyling some elements in style.css, redoing a function to call results in app.js.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,58 +6,13 @@ var correct;
 var inCorrect;
 var timeUp;
 
-// Object containing questions and answers
-var myQuestions = [{
-  ques: "Who is Harry Potter's Godfather?",
-  ans: ["Severus Snape", "Sirius Black", "Remus Lupin", "Albus Dumbledore"],
-  name: "godfather",
-  correct: "Sirius Black",
-  divClass: ".godfather"
-},]
+//
 
-var myQuestions = [{
-  ques: "What is the name of the elf that saves Harry's life?",
-  ans: ["Winky", "Dobby", "Kreacher", "Hedwig"],
-  name: "elf",
-  correct: "Dobby",
-  divClass: ".elf"
-},]
-
-var myQuestions = [{
-  ques: "How many Weasley children are there?",
-  ans: [8, 7, 6, 9],
-  name: "weasley",
-  correct: 7,
-  divClass: ".weasley"
-},]
-
-var myQuestions = [{
-  ques: "What is the message written on the snitch Dumbledore gave to Harry?",
-  ans: ["I open at the close.", "With great power comes great responsibility", "Death is but the next great adventure.", "I solemnly swear that I am up to no good."],
-  name: "snitch",
-  correct: "I open at the close.",
-  divClass: ".snitch"
-},]
-
-var myQuestions = [{
-  ques: "What is the name of the dark wizard Harry must defeat?",
-  ans: ["Grindelwald", "Draco Malfoy", "Sirius Black", "Voldemort"],
-  name: "wizard",
-  correct: "Voldemort",
-  divClass: ".wizard"
-},] // end the object.
-
-
-var myAnswer = [""]
-
-
-// setTimeout 
-setTimeout(timeUp, 1000 * 60);
 
 // Create Functions
 // ============================================
-// Start countdown on load
-var startGame = $("#start-btn").on("click",  function () {
+// Start countdown on click
+  var startGame = $("#start-btn").on("click", function () {
   var seconds = 60;
   var intervalId;
   var correct;
@@ -92,11 +47,28 @@ var startGame = $("#start-btn").on("click",  function () {
     alert("Time's up!");
   }
 
- 
 });
+
+// Function to show results
+$("#submit-btn").on("click", function onSubmit() {
+  var score = 0;
+  var numOfQuestions = 5;
+  var answerArray = ['b', 'b', 'b', 'a', 'd'];
+
+  // get the quiz
+  var q1 = document.forms['quiz']['q1'].value;
+  var q1 = document.forms['quiz']['q1'].value;
+  var q1 = document.forms['quiz']['q1'].value;
+  var q1 = document.forms['quiz']['q1'].value;
+  var q1 = document.forms['quiz']['q1'].value;
+})
+
 
 // Main Processes 
 // ============================================
+// setTimeout 
+setTimeout(timeUp, 1000 * 60);
+
 // Start countdown 
 $("#time-left").text("00:60");
 
