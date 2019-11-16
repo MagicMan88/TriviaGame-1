02a50bb413978bf8531adf835a8ad8f0932e1559
Stephen Green <stephengreen1028@gmail.com>
2019-11-15 18:29:07 -0700

    Scrapped index.html, style.css and app.js due to issues with app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,103 +1,90 @@
-// Declare variables
-// ============================================
-var seconds = 60;
-var intervalId;
-var timeUp;
-
-//
-
-
-// Create Functions
-// ============================================
-// Start countdown on click
-  var startGame = $("#start-btn").on("click", function () {
-  var seconds = 60;
-  var intervalId;
-  var timeUp;
-
-  setTimeout(timeUp, 1000 * 60);
-
-  // Start function 
-  function start() {
-    intervalId = setInterval(count, 1000);
+var card = $("#quiz");
+// Question set
+var questions = [
+  {
+    question: "Who is Harry Potter's Godfather?",
+    answers: ["Sirius Black", "Severus Snape", "Remus Lupin", "Albus Dumbledore"],
+    correctAnswer: "Sirius Black"
+  },
+  {
+    question: "What is the name of the elf that saves Harry's life?",
+    answers: ["Winky", "Dobby", "Kreacher", "Hedwig"],
+    correctAnswer: "Dobby"
+  },
+  {
+    question: "How many Weasley children are there?",
+    answers: [6,7,8,9],
+    correctAnswer: 7
+  },
+  {
+    question: "What is the message written on the snitch Dumbledore gave to Harry?",
+    answers: ["I open at the close", "With great power comes great responsibility", "Death is but the next great adventure", "I solemnly swear that I am up to no good"],
+    correctAnswer: "I open at the close"
+  },
+  {
+    question: "What is the name of the dark wizard Harry must defeat?",
+    answers: ["Grindelwald", "Draco Malfoy", "Sirius Black", "Voldemort"],
+    correctAnswer: "Voldemort"
+  },
+  {
+    question:
+      "Finish this line from the Fresh Prince of Bel-Air theme song: 'I whistled for a cab and when it came near, the license plate said...'",
+    answers: ["Dice", "Mirror", "Fresh", "Cab"],
+    correctAnswer: "Fresh"
   }
-
-  // Count function
-  function count() {
-    // DONE: increment time by 1, remember we cant use "this" here.
-    seconds--;
-    var converted = timeConverter(seconds);
-    console.log(converted);
-    // DONE: Use the variable we just created to show the converted time in the "display" div.
-    $("#time-left").text(converted);
-  }
-  start();
-
-  // Time's up function
-  function timeUp() {
-    $("#time-left").text("Time's up!");
-    clearInterval(intervalId);
-    // Alert when time's up
-    alert("Time's up!");
-  }
-
-});
-
-// Function to show results
-$("#submit-btn").on("click", function onSubmit() {
-  var score = 0;
-  var numOfQuestions = 5;
-  var answerArray = ['b', 'b', 'b', 'a', 'd'];
-
-  // get the quiz
-  var q1 = document.forms['quiz']['q1'].value;
-  var q1 = document.forms['quiz']['q1'].value;
-  var q1 = document.forms['quiz']['q1'].value;
-  var q1 = document.forms['quiz']['q1'].value;
-  var q1 = document.forms['quiz']['q1'].value;
-
-  // Loop through answers
-  for (var i = 1; i <= numOfQuestions; i++){
-    if(eval('q' + i) == '') {
-      alert("You messed up question " + i);
+];
+// Variable that will hold the timer
+var timer;
+var game = {
+  correct: 0,
+  incorrect: 0,
+  counter: 90,
+  countdown: function() {
+    game.counter--;
+    $("#counter-number").html(game.counter);
+    if (game.counter === 0) {
+      console.log("TIME UP");
+      game.done();
     }
-
-    for (var i = 1; i <= numOfQuestions; i++){
-      if(eval('q' + i) == answerArray[i - 1]) {
-        score++;
+  },
+  start: function() {
+    timer = setInterval(game.countdown, 1000);
+    $("#sub-wrapper").prepend(
+      "<h2>Time Remaining: <span id='counter-number'>120</span> Seconds</h2>"
+    );
+    $("#start").remove();
+    for (var i = 0; i < questions.length; i++) {
+      card.append("<h2>" + questions[i].question + "</h2>");
+      for (var j = 0; j < questions[i].answers.length; j++) {
+        card.append("<input type='radio' name='question-" + i +
+          "' value='" + questions[i].answers[j] + "''>" + questions[i].answers[j]);
       }
     }
-
-    // Add results to html
-    var results = $("#results")
-    results.text("<h2> You messed up </h2>")
-    alert("You scored " + score + " out of " (numOfQuestions)
+    card.append("<button id='done'>Done</button>");
+  },
+  done: function() {
+    var inputs = card.children("input:checked");
+    for (var i = 0; i < inputs.length; i++) {
+      if ($(inputs[i]).val() === questions[i].correctAnswer) {
+        game.correct++;
+      } else {
+        game.incorrect++;
+      }
+    }
+    this.result();
+  },
+  result: function() {
+    clearInterval(timer);
+    $("#sub-wrapper h2").remove();
+    card.html("<h2>All Done!</h2>");
+    card.append("<h3>Correct Answers: " + this.correct + "</h3>");
+    card.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
   }
-})
-
-
-// Main Processes 
-// ============================================
-// setTimeout 
-setTimeout(timeUp, 1000 * 60);
-
-// Start countdown 
-$("#time-left").text("00:60");
-
-function timeConverter(t) {
-
-  var minutes = Math.floor(t / 60);
-  var seconds = t - (minutes * 60);
-
-  if (seconds < 10) {
-    seconds = "0" + seconds;
-  }
-
-  if (minutes === 0) {
-    minutes = "00";
-  } else if (minutes < 10) {
-    minutes = "0" + minutes;
-  }
-
-  return minutes + ":" + seconds;
-}
+};
+// CLICK EVENTS
+$(document).on("click", "#start", function() {
+  game.start();
+});
+$(document).on("click", "#done", function() {
+  game.done();
+});
